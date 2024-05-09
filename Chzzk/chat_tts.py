4410736a40e5825d2dd9013244bfccba9a572696
마인머스(석민주) <84371372+vmintf@users.noreply.github.com>
2024-05-09 22:04:27 +0900

    Update chat_tts.py

--- Chzzk/chat_tts.py
+++ Chzzk/chat_tts.py
@@ -3,7 +3,7 @@ import time
 import requests
 import websocket
 import json
-import random
+import threading
 
 from playsound import playsound
 from gtts import gTTS
@@ -15,10 +15,11 @@ class ChnConnect(websocket.WebSocketApp):
                          on_open=self.on_open,
                          on_error=self.on_error,
                          on_close=self.on_close,
-                         on_message=self.on_message,
-                         on_data=self.on_data)
+                         on_message=self.on_message)
         self.acc_token = acc_token
         self.cht_id = cht_id
+        self.pinged = False
+        websocket.enableTrace(False)
 
     def data_case(self, x):
         match x:
@@ -33,9 +34,25 @@ class ChnConnect(websocket.WebSocketApp):
                 data = {"ver": "2", "cmd": 10000}
                 return json.dumps(data)
 
-            case 2:  # 핑퐁 2
+            case 3:  # 핑퐁 2
                 data = {"ver": "2", "cmd": 0}
                 return json.dumps(data)
+
+
+    def unplayable_letters(self, x):
+        match x:
+            case "?":
+                return "물음표"
+            case "ㅏ":
+                return "아"
+            case "ㅓ":
+                return "어"
+            case "ㅜ":
+                return "우"
+            case "ㅔ":
+                return "에"
+            case "ㅐ":
+                return "애"
 
     def on_open(self, ws):
         print("connected")
@@ -49,25 +66,53 @@ class ChnConnect(websocket.WebSocketApp):
         print("### closed ###")
 
     def on_message(self, ws, message):
-        print(message)
-        match json.loads(message):
-            case {"ver": "2", "cmd": 0}:
-                return ws.send(self.data_case(2))
+        # print(json.loads(message))
+        # print("bdy" in json.loads(message))
+        if "bdy" in json.loads(message):
+            # print(json.loads(message)["bdy"][0]['msg'])
+            # print("msg" in json.loads(message)["bdy"][0])
+            if "msg" in json.loads(message)["bdy"][0]:
+                # print("msg" in json.loads(message)["bdy"][0])
+                self.tts(json.loads(message)["bdy"][0]["msg"])
+        elif {"ver": "2", "cmd": 0} == json.loads(message):
+            print("[핑] 서버 연결 유지를 위해, 핑을 전송합니다.")
+            self.send(data=self.data_case(2))
 
-            case {"ver": "2", "cmd": 10000}:
-                return ws.send(self.data_case(3))
+    def tts(self, msg):
+        print(f"[TTS 플레이] {msg}")
+        if msg[0] in ["ㅏ", "ㅓ", "ㅔ", "ㅐ", "?", "ㅜ"]:
+            g = gTTS(text=f"{self.unplayable_letters(msg[0])}, {msg[1] if len(msg) == 1 else None}", lang="ko", slow=False)
+            urd = os.urandom(16)
+            urd_str = str(int.from_bytes(urd, 'little'))[:4]
+            g.save(f"./tmp/tmp_{urd_str}.mp3")
+            playsound(sound=f"./tmp/tmp_{urd_str}.mp3")
+            os.remove(f"./tmp/tmp_{urd_str}.mp3")
+            return
+        else:
+            g = gTTS(text=str(msg), lang="ko", slow=False)
+            urd = os.urandom(16)
+            urd_str = str(int.from_bytes(urd, 'little'))[:4]
+            g.save(f"./tmp/tmp_{urd_str}.mp3")
+            playsound(sound=f"./tmp/tmp_{urd_str}.mp3")
+            os.remove(f"./tmp/tmp_{urd_str}.mp3")
+            return
 
-    def on_data(self, ws, message, flags, opcode):
-        if json.loads(message)["bdy"][0]["msg"]:
-            g = gTTS(
-                text=json.loads(message)["bdy"][0]["msg"],
-                lang="ko",
-                slow=False
-            )
-            rand = str(int.from_bytes(os.urandom(16), "little"))[:5]
-            g.save(f"./tmp/tmp_{rand}.mp3")
-            playsound(f'{os.getcwd()}/tmp/tmp_{rand}.mp3')
-            os.remove(f"./tmp/tmp_{rand}.mp3")
+    # def ping(self):
+    #     set_interval: int = 20
+    #     while True:
+    #         if self.pinged is False:
+    #             time.sleep(set_interval)
+    #             self.send(self.data_case(3))
+    #             self.pinged = True
+    #             print("서버 연결 유지를 위해 데이터를 전송합니다.")
+
+    def connect(self):
+        try:
+            # t = threading.Thread(target=self.ping)
+            # t.start()
+            self.run_forever(origin="https://chzzk.naver.com", reconnect=1)
+        except websocket.WebSocketConnectionClosedException:
+            exit(0)
 
 
 def __api_url(x: int):
@@ -108,10 +153,10 @@ def get_chat_access_token(chnid) -> dict:
 
 if __name__ == "__main__":
     for i in os.listdir("./tmp/"):
-        os.remove(i)
+        os.remove(f"./tmp/{i}")
 
-    gls = get_live_status("0441c4d35b2668ebaa0906302665adfc")["content"]["chatChannelId"]
-    gcat = get_chat_access_token("0441c4d35b2668ebaa0906302665adfc")["content"]["accessToken"]
+    gls = get_live_status("4fa8d17010a569f7739b611da4a6edd2")["content"]["chatChannelId"]
+    gcat = get_chat_access_token("4fa8d17010a569f7739b611da4a6edd2")["content"]["accessToken"]
 
     c = ChnConnect(url=__api_url(3), acc_token=gcat, cht_id=gls)
-    c.run_forever(origin="https://chzzk.naver.com")
+    c.connect()
